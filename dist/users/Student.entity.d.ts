833c5f3af9ddb03c0ceba55fdef12448bf2ef4b8
JOELGANIZANI75 <167243130+JOELGANIZANI75@users.noreply.github.com>
2025-09-04 16:10:58 +0200

    Add files via upload

---
+++ dist/users/Student.entity.d.ts
@@ -0,0 +1,14 @@
+import { BaseEntity } from 'typeorm';
+import { Bonding } from '../bonding/bonding-entity';
+export declare class User extends BaseEntity {
+    id: number;
+    RegNo: string;
+    FirstName: string;
+    SurName: string;
+    University: string;
+    email: string;
+    password: string;
+    resetPasswordToken: string;
+    resetPasswordExpires: Date;
+    bondings: Bonding[];
+}
